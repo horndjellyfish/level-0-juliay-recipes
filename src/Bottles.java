793f902fcf900d73eb4819a4b8d2f565d0a11507
horndjellyfish <league@WTS-MM-7.local>
2017-05-03 18:30:12 -0700

    Day 2

---
+++ src/Bottles.java
@@ -0,0 +1,10 @@
+
+public class Bottles {
+public static void main(String[] args) {
+ for (int i = 99; i > 0; i-=1) {
+int bottles = i - 1;
+System.out.println(i+" bottles of beer on the wall," + i+" bottles of beer,");
+System.out.println(" Take one down, pass it around," + bottles +" bottles of beer on the wall!");
+ }	
+}
+}
---
+++ src/Change.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Change {
+	public static void main(String[] args) {
+
+		// Ask the user how many nickels they have
+JOptionPane.showInputDialog("How many nickels do you have?");
+		// Convert their answer to an int using Integer.parseInt()
+
+		// Ask the user how many dimes they have, and convert their answer
+
+		// Ask the user how many quarters they have, and convert their answer
+
+		// Calculate how much money the user has and save it in a double variable 
+
+		// Tell the user how much money they have
+
+	}
+
+}
