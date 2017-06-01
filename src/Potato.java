c00fd750aadad510f9e8bb1860ff5490d22d2f23
horndjellyfish <league@WorthyMini-2.local>
2017-05-31 18:35:07 -0700

    Day 6

---
+++ src/Potato.java
@@ -0,0 +1,13 @@
+
+public class Potato {
+public static void main(String[] args) {
+	for (int l = 1; l < 9; l+=1) {
+	int potato = l + 1;
+	System.out.println(l+ " potato");
+	
+}
+	if (l == 4) {
+	System.out.println(l);
+}
+}
+}
