04377f1a555a82e1bb1cc29a88dade32f833f116
tactreconx <league@Admins-Mac-mini.local>
2018-01-20 11:26:48 -0800

    Day 29
    Goofy Names
    unfinished

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -15,25 +15,28 @@ public class GoofyNames {
 		int i;
 		int length = name.length();
 		for (i = 0; i < length-1; i+=1); {
-			String mid = name.substring(i, i+1);
-			System.out.println(mid);
+			String current = name.substring(i, i+1); 
+			//System.out.println(current);
 		}
 			//    HINT: replace 'start' and 'end' to get String with character at i 
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-		String mid = name.substring(i, i+1); 
-			if (i % 2 == 0) {
-				System.out.println(name.toUpperCase());
-				
-			}
-			else {
-				System.out.println(mid);
-			}
 		
+			String current = name.substring(i, i+1); 
+		
+			if (i+1 % 2 == 0) {
+				System.out.println(current.toUpperCase());
+				
+			} 
+			else { 
+				System.out.println(current);
+			}
+		 
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 		
 		// 7. Use pop-up to show user their Goofy name
 	}
 }
+
 
