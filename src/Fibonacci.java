3230052efa338ab3682e7c5ac0943389e1f8ceee
League <league@Mac-mini-4.local>
2018-02-03 11:26:57 -0800

    Day 31: Fibonacci - finished

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,17 @@
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		for(int i = 1; i < 145; i += i + i - 1) {
-			System.out.println(i);
+		for (int i = 0; i < 12; i += 1) {
+			System.out.print(fib(i) + " ");
 		}
-	}	
+	}
+	private static int fib(int num) {
+		if (num == 0) {
+			return 1;
+		}
+		else if (num == 1) {
+			return 1;
+		}
+		return fib(num - 2) + fib(num - 1);
+	}
 }
--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -7,7 +7,8 @@ public class PrimeOrNot {
 	if (i == 2) {
 		JOptionPane.showMessageDialog(null, "2 is a prime number.");
 	}
-	for (int abc = 2; abc < i; abc += 1) {
+	for (int abc = 2; abc < i; abc += 1);
+		int abc = 2;
 			if (i % abc <= 0) {
 			JOptionPane.showMessageDialog(null, i + " is not a prime number; it is composite.");	
 			} else {
@@ -19,4 +20,4 @@ public class PrimeOrNot {
 	
 	}
 
-}
+
